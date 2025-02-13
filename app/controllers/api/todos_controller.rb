1d7f1ba21d7af80033cceb41a6255a2f982298d8
yg <yg@anadeainc.com>
2025-02-13 22:31:09 +0100

    Add backend part of the app

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,19 +1,16 @@
 class Api::TodosController < ApplicationController
   before_action :set_todo, only: %i[ show update_completed destroy ]
 
-  # GET /todos
   def index
     @todos = Todo.all
 
     render json: @todos
   end
 
-  # GET /todos/1
   def show
     render json: @todo
   end
 
-  # POST /todos
   def create
     @todo = Todo.new(todo_params)
 
@@ -24,7 +21,6 @@ class Api::TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
   def update_completed
     if @todo.update(completed: params[:completed])
       render json: @todo
@@ -33,18 +29,15 @@ class Api::TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
   def destroy
     @todo.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:todo_name, :completed)
     end
