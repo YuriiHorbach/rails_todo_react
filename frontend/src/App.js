5e6040082e07f43061a7a370e293e627e6e6f98e
YuriiHorbach <yuriihorbach.career@gmail.com>
2025-02-15 17:24:31 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-container">
+        <h1 className="title"> Todo App </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
